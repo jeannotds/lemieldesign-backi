4acaed345d913ea1ad4879472d7a62fa29788db3
Jeannot Diambu <diambujeannot@gmail.com>
2025-08-25 12:00:43 +0100

    clean code

--- src/collections/collections.controller.ts
+++ src/collections/collections.controller.ts
@@ -1,17 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CollectionDto } from 'src/dto/create-collection.dto';
 import { CollectionsService } from './collections.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('collections')
 export class CollectionsController {
 
   constructor(private readonly collectionsService: CollectionsService){}
   
-
+    // --- Route protégée ---
+  @UseGuards(AuthGuard('jwt'))
   @Post('/add')
   async create(@ Body() cretaeCollection: CollectionDto){
     return this.collectionsService.addCollection(cretaeCollection)
-
   }
 
 }
